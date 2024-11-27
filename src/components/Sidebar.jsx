06576ab537f88305e7a3c6baf61fea86e8ec309f
suwitcha <kobe1234.aaa@gmail.com>
2024-11-27 22:12:44 +0700

    sidebaar css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,32 +1,44 @@
-// src/components/Sidebar.js
 import React from 'react';
+import '../css/Sidebar.css';
+import { FaBookmark, FaCog, FaSearch } from 'react-icons/fa';
 
 const Sidebar = ({ user, isAdmin, handleProfileClick, handleAdminClick }) => {
   return (
     <div className="sidebar">
       <div className="profile-container">
-        <button className="profile-btn1" onClick={() => handleProfileClick(user.id)}>
+        <button className="profile-btn" onClick={() => handleProfileClick(user.id)}>
           <img 
             src={user.profilePicture || '/default-profile.png'} 
             alt="Profile" 
-            className="profile-image1" 
+            className="profile-image" 
           />
         </button>
-        <div className="user-name" onClick={() => handleProfileClick(user.id)}>
-          {user.firstName + ' ' + user.lastName}
+        <div className="user-info" onClick={() => handleProfileClick(user.id)}>
+          <div className="user-name">{user.firstName} {user.lastName}</div>
+          <div className="user-role">{user.role}</div>
         </div>
       </div>
       {/* If the user is an admin, display a link to admin management page */}
       {isAdmin && (
         <div className="admin-link" onClick={handleAdminClick}>
-          <img 
-            className="admin-icon" 
-            src="https://cdn-icons-png.flaticon.com/512/78/78948.png" 
-            alt="Admin Icon" 
-          />
-          <p className="admin-link">Admin Management</p>
+          <FaCog className="admin-icon" />
+          <span className="admin-link-text">Admin Management</span>
         </div>
       )}
+      <div className="sidebar-nav">
+        <div className="nav-item">
+          <FaSearch className="nav-icon" />
+          <span className="nav-text">Search</span>
+        </div>
+        <div className="nav-item">
+          <FaBookmark className="nav-icon" />
+          <span className="nav-text">Bookmarks</span>
+        </div>
+        <div className="nav-item">
+          <FaCog className="nav-icon" />
+          <span className="nav-text">Settings</span>
+        </div>
+      </div>
     </div>
   );
 };
